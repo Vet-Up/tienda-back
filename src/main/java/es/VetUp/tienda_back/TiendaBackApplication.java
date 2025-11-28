11a3b8e0a8bd4f6b820cda98468763a99969bbed
Alejandro M <mapi1210ceo@gmail.com>
2025-11-28 13:08:53 +0100

    Estructura aplicacion back añadida

---
+++ src/main/java/es/VetUp/tienda_back/TiendaBackApplication.java
@@ -0,0 +1,13 @@
+package es.VetUp.tienda_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaBackApplication.class, args);
+	}
+
+}
