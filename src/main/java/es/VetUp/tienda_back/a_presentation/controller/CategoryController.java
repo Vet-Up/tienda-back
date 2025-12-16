8ab94c9845a7aa6b14128487b11222e8d3e23379
alejandroo-16 <mapi1210ceo@gmail.com>
2025-12-16 18:59:34 +0100

    Added crossOrigin category

--- src/main/java/es/VetUp/tienda_back/a_presentation/controller/CategoryController.java
+++ src/main/java/es/VetUp/tienda_back/a_presentation/controller/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
     public final CategoryService categoryService;
